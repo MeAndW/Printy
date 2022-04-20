3345b81ae4d863f4eeb67874d3f3ce50cf1bf6c3
MeAndW <st1672@roong-aroon.ac.th>
2022-04-20 18:28:07 +0700

    updated

--- printy.hpp
+++ printy.hpp
@@ -1,4 +1,5 @@
 // A helper library for printing various objects.
+#pragma once
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -6,14 +7,18 @@
 #include <type_traits>
 #include <utility>
 #include <functional>
+#include <algorithm>
 #include <concepts>
 #include <ranges>
+
+#include <variant>
+#include <optional>
 
 #include <string_view>
 /**
  * @brief Credit to 
  * https://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c/58331141#58331141
- * This needs <string_view> to work.
+ * Require <string_view>.
  */
 namespace typeview
 {
@@ -61,12 +66,185 @@ namespace typeview
                            << std::string(sizeof(#x), '-') << '\n'
 namespace printy
 {
-    template<typename CharT, typename Traits, typename T>
+    template<typename T, typename CharT, typename Traits>
     concept insertable = requires(std::basic_ostream<CharT, Traits>& os, const T& t)
     {
         { os << t } -> std::same_as<std::basic_ostream<CharT, Traits>&>;
     };
+    template<typename T, typename CharT, typename Traits>
+    concept uninsertable = !insertable<T, CharT, Traits>;
 
+    template<typename R, typename CharT, typename Traits>
+    concept insertable_range = 
+        std::ranges::input_range<R> &&
+        insertable<std::ranges::range_value_t<R>, CharT, Traits>;
+
+    template<typename R>
+    concept nested_input_range = 
+        std::ranges::input_range<R> &&
+        std::ranges::input_range<std::ranges::range_value_t<R>>;
+
+    template<typename>
+    struct is_tuple : std::false_type {};
+    template<typename... Ts>
+    struct is_tuple<std::tuple<Ts...>> : std::true_type {};
+    template<typename T>
+    constexpr bool is_tuple_v = is_tuple<T>::value;
+
+    template<typename>
+    struct is_pair : std::false_type {};
+    template<typename T, typename U>
+    struct is_pair<std::pair<T, U>> : std::true_type {};
+    template<typename T>
+    constexpr bool is_pair_v = is_pair<T>::value;
+
+    template<typename>
+    struct is_string : std::false_type {};
+    template<typename CharT, typename Traits, typename Alloc>
+    struct is_string<std::basic_string<CharT, Traits, Alloc>> : std::true_type {};
+    template<typename T>
+    constexpr bool is_string_v = is_string<T>::value;
+
+    template<typename>
+    struct is_string_view : std::false_type {};
+    template<typename CharT, typename Traits>
+    struct is_string_view<std::basic_string_view<CharT, Traits>> : std::true_type {};
+    template<typename T>
+    constexpr bool is_string_view_v = is_string_view<T>::value;
+
+
+    template<typename T>
+    constexpr bool is_string_or_string_view_v = is_string_v<T> || is_string_view_v<T>;
+
+    template<typename T>
+    constexpr bool is_string_or_string_view_or_cs_v = is_string_or_string_view_v<T> || std::is_same_v<T, const char*>;
+
+    template<typename T>
+    concept string_or_string_view = is_string_or_string_view_v<T>;
+
+    template<typename T>
+    concept string_type = is_string_or_string_view_or_cs_v<T>;
+
+    template<typename T>
+    concept container_sup_nest = 
+        !string_type<T> &&
+        (std::ranges::input_range<T> || is_tuple_v<T>);
+
+    // Use this namespace in the global scope to let its overloads be used everywhere.
+    namespace general 
+    {
+        static unsigned int indent_level;
+
+        template<typename CharT, typename Traits>
+        static constexpr void apply_indent(int level_change, std::basic_ostream<CharT, Traits>& os)
+        {
+            indent_level += level_change;
+            os << '\n';
+            for (unsigned int i = 0, n = indent_level * 4; i < n; i++)
+                os << ' ';
+        }
+        
+        /**
+         * @brief The overload responsible for inserting a range. 
+         * Normally, if the inserted range is a range of ranges, a special style is applied. 
+         * `std::string` and `std::string_view` in particular are excluded from this special style.
+         * 
+         * @tparam CharT 
+         * @tparam Traits 
+         * @tparam R 
+         * @param os 
+         * @param r 
+         * @return std::basic_ostream<CharT, Traits>& 
+         */
+        template<typename CharT, typename Traits, insertable_range<CharT, Traits> R>
+        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const R& r)
+        {
+            using value_type = std::ranges::range_value_t<R>;
+            constexpr bool use_indent = container_sup_nest<value_type>;
+            os << '{';
+            if constexpr (use_indent)
+                apply_indent(+1, os);
+            bool first = true;
+            for (auto&& e : r)
+            {
+                if (first)
+                    first = false;
+                else
+                {
+                    os << ", ";
+                    if constexpr (use_indent)
+                        apply_indent(0, os);
+                }
+                os << e;
+            }
+            if constexpr (use_indent)
+                apply_indent(-1, os);
+            os << '}';
+            return os;
+        }
+
+        template <typename CharT, typename Traits, insertable<CharT, Traits>... Args>
+        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const std::tuple<Args...>& tuple)
+        {
+            constexpr bool use_indent = (container_sup_nest<Args> || ...);
+            os << '(';
+            if constexpr (use_indent)
+                apply_indent(+1, os);
+            std::apply([&](const auto&... args) {
+                bool first = true;
+                ([&](const auto& arg) {
+                    if (first)
+                        first = false;
+                    else
+                    {
+                        os << ", ";
+                        if constexpr (use_indent)
+                            apply_indent(0, os);
+                    }
+                    os << arg;
+                }(args), ...);
+            }, tuple);
+            if constexpr (use_indent)
+                apply_indent(-1, os);
+            os << ')';
+            return os;
+        }
+
+        template <typename CharT, typename Traits, insertable<CharT, Traits> T, insertable<CharT, Traits> U>
+        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const std::pair<T, U>& pair)
+        {
+            return os << '(' << pair.first << ": " << pair.second << ')';
+        }
+
+        template<typename CharT, typename Traits>
+        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const std::monostate&)
+        {
+            os << "std::monostate";
+            return os;
+        }
+
+        template<typename CharT, typename Traits, insertable<CharT, Traits> Arg, insertable<CharT, Traits>... Args>
+        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const std::variant<Arg, Args...>& variant)
+        {
+            if (variant.valueless_by_exception())
+                return os << "valueless_by_exception";
+            std::visit([&](const auto& arg) {
+                os << arg;
+            }, variant);
+            return os;
+        }
+
+        template<typename CharT, typename Traits, insertable<CharT, Traits> Arg>
+        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const std::optional<Arg>& optional)
+        {
+            if (optional.has_value())
+                os << optional.value();
+            else
+                os << "std::nullopt";
+            return os;
+        }
+
+    } // namespace general
 
     /**
      * @brief printy::custom 
@@ -86,10 +264,18 @@ namespace printy
 
             template<typename CharT, typename Traits>
             friend std::basic_ostream<CharT, Traits>& 
-            operator<<(std::basic_ostream<CharT, Traits>& os, const Indentation& indent);
+            operator<<(std::basic_ostream<CharT, Traits>& os, const Indentation& indent)
+            {
+                os << '\n';
+                for (unsigned int i = 0, n = indent.level * indent.size; i < n; i++)
+                    os << indent.fill;
+                return os;
+            }
 
-            Indentation& operator++() noexcept { ++level; return *this; }
-            Indentation& operator--() noexcept { --level; return *this; }
+            Indentation operator++() noexcept { ++level; return *this; }
+            Indentation operator--() noexcept { --level; return *this; }
+            Indentation operator++(int) noexcept { return Indentation(level++, size, fill); }
+            Indentation operator--(int) noexcept { return Indentation(level--, size, fill); }
         };
 
         struct Punctuation 
@@ -113,12 +299,19 @@ namespace printy
         public:
             typed_ostream(std::basic_ostream<CharT, Traits>& os, const Punctuation& punctuation = Punctuation("", "(", ")")) : 
             os(os), punctuation(punctuation) {}
-
-            template<typename T> typed_ostream& operator<<(const T& t)
+            
+            template<insertable<CharT, Traits> T> typed_ostream& operator<<(const T& t)
             {
                 os << punctuation.prefix << typeview::type_view<T>() << punctuation.delimiter << t << punctuation.suffix;
                 return *this;
             }
+            template<uninsertable<CharT, Traits> T> typed_ostream& operator<<(const T& t)
+            {
+                os << typeview::type_view<T>();
+                return *this;
+            }
+            
+
 
             typed_ostream& operator<<(typed_ostream& (*pf)(typed_ostream&))
             { return pf(*this); }
@@ -136,151 +329,5 @@ namespace printy
             }
         };
     } // namespace custom
-
-    namespace general 
-    {
-        struct Settings
-        {
-            static constexpr unsigned int indent_size = 4;
-            static constexpr char indent_fill = ' ';
-
-            static constexpr bool range_use_indent = true;
-            static constexpr char range_prefix[] = "{";
-            static constexpr char range_delimiter[] = ", ";
-            static constexpr char range_suffix[] = "}";
-
-            static constexpr bool tuple_use_indent = false;
-            static constexpr char tuple_prefix[] = "(";
-            static constexpr char tuple_delimiter[] = ", ";
-            static constexpr char tuple_suffix[] = ")";
-
-            static constexpr bool pair_use_indent = false;
-            static constexpr char pair_prefix[] = "";
-            static constexpr char pair_delimiter[] = ": ";
-            static constexpr char pair_suffix[] = "";
-
-            static unsigned int indent_level;
-
-            template<typename CharT, typename Traits>
-            static constexpr void apply_indent(int level_change, std::basic_ostream<CharT, Traits>& os)
-            {
-                indent_level += level_change;
-                os << '\n';
-                for (unsigned int i = 0, n = indent_level*indent_size; i < n; i++)
-                    os << indent_fill;
-            }
-        };
-        unsigned int Settings::indent_level = 0;
-
-        /**
-         * @brief The overload responsible for inserting a range. 
-         * Normally, if the inserted range is a range of ranges, a special style is applied. 
-         * `std::string` and `std::string_view` in particular are excluded from this special style.
-         * 
-         * @tparam CharT 
-         * @tparam Traits 
-         * @tparam R 
-         * @param os 
-         * @param r 
-         * @return std::basic_ostream<CharT, Traits>& 
-         */
-        template<typename CharT, typename Traits, std::ranges::range R>
-        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const R& r)
-        {
-            typedef std::ranges::range_value_t<R> value_type;
-            constexpr bool use_indent = std::ranges::range<value_type> && Settings::range_use_indent && !std::derived_from<value_type, std::string> && !std::derived_from<value_type, std::string_view>;
-
-            os << Settings::range_prefix;
-            if constexpr (use_indent)
-                Settings::apply_indent(+1, os);
-            bool first = true;
-            for (auto&& e : r)
-            {
-                if (first)
-                    first = false;
-                else
-                {
-                    os << Settings::range_delimiter;
-                    if constexpr (use_indent)
-                        Settings::apply_indent(0, os);
-                }
-                os << e;
-            }
-            if constexpr (use_indent)
-                Settings::apply_indent(-1, os);
-            os << Settings::range_suffix;
-            return os;
-        }
-
-        template <typename CharT, typename Traits, typename... Args>
-        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const std::tuple<Args...>& tuple)
-        {
-            os << Settings::tuple_prefix;
-            if (Settings::tuple_use_indent)
-                Settings::apply_indent(+1, os);
-            bool first = true;
-            std::apply([&os, &first](auto &&...args) {
-                auto print = [&] (auto&& val) 
-                {
-                    if (first)
-                        first = false;
-                    else
-                        os << Settings::tuple_delimiter;
-                    os << val;
-                };
-                (print(args), ...); 
-            }, tuple);
-            if (Settings::tuple_use_indent)
-                Settings::apply_indent(-1, os);
-            os << Settings::tuple_suffix;
-            return os;
-        }
-
-        template <typename CharT, typename Traits, typename K, typename V>
-        std::basic_ostream<CharT, Traits>& operator<<(std::basic_ostream<CharT, Traits>& os, const std::pair<K, V>& pair)
-        {
-            os << Settings::pair_prefix;
-            if (Settings::pair_use_indent)
-                Settings::apply_indent(+1, os);
-            os << pair.first << Settings::pair_delimiter << pair.second;
-            if (Settings::pair_use_indent)
-                Settings::apply_indent(-1, os);
-            os << Settings::pair_suffix;
-            return os;
-        }
-        
-        template<typename CharT, typename Traits>
-        class typed_ostream
-        {
-            using Punctuation = custom::Punctuation;
-        private:
-            std::basic_ostream<CharT, Traits>& os;
-            const Punctuation punctuation;
-        public:
-            typed_ostream(std::basic_ostream<CharT, Traits>& os, const Punctuation& punctuation = Punctuation("", "(", ")")) : 
-            os(os), punctuation(punctuation) {}
-
-            template<typename T> typed_ostream& operator<<(const T& t)
-            {
-                os << punctuation.prefix << typeview::type_view<T>() << punctuation.delimiter << t << punctuation.suffix;
-                return *this;
-            }
-
-            typed_ostream& operator<<(typed_ostream& (*pf)(typed_ostream&))
-            { return pf(*this); }
-
-            typed_ostream& operator<<(std::ostream& (*pf)(std::ostream&))
-            { 
-                pf(os);
-                return *this;
-            }
-
-            typed_ostream& operator<<(std::ios_base& (*pf)(std::ios_base&))
-            { 
-                pf(os);
-                return *this;
-            }
-        };
-    } // namespace general
 
 } // namespace printy
--- test.cpp
+++ test.cpp
@@ -51,6 +51,18 @@
 
 using namespace std;
 
+static_assert(ranges::range<vector<int>>);
+static_assert(not ranges::viewable_range<vector<int>>);
+static_assert(printy::insertable_range<vector<int>, char, char_traits<char>>);
+static_assert(not ranges::view<vector<int>>);
+static_assert(printy::container_sup_nest<vector<int>>);
+static_assert(printy::container_sup_nest<tuple<int, int>>);
+static_assert(not printy::container_sup_nest<char>);
+static_assert(printy::string_or_string_view<string>);
+static_assert(printy::string_or_string_view<string_view>);
+
+using namespace printy::general;
+
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     array<int, 5> arr2 = {1, 2, 3, 4, 5};
@@ -62,19 +74,24 @@ int main() {
             {13, 14, 15, 16}
         }
     };
-
-
-    using namespace printy::general;
-
+    variant<int, string> v = "hello";
+    variant<monostate, double, char> v2 = monostate{};
 
     cout << arr << endl;
     cout << arr2 << endl;
     cout << nested << endl;
-    printy::general::typed_ostream os(cout);
+    cout << v << endl;
+    cout << v2 << endl;
+    printy::custom::typed_ostream os(cout);
 
     os << arr << endl;
     os << arr2 << endl;
     os << nested << endl;
+    os << v << endl;
+    os << v2 << endl;
+    os << "hello" << endl;
+    os << string{"hello"} << endl;
+    os << string_view{"\\"} << endl;
     os << tuple<int, int, int>{1, 2, 3} << endl;
     os << make_tuple(1, 2, 
         vector<double>{3.0, 4.0, 5.0},
@@ -90,9 +107,24 @@ int main() {
             make_tuple("world", 2),
         }
     ) << endl;
-    os << std::map<int, string>{
+    os << map<int, string>{
         {1, "one"},
         {2, "two"},
         {3, "three"},
     } << endl;
+    os << map<int, tuple<int, int, int>>{
+        {1, {1, 2, 3}},
+        {2, {4, 5, 6}},
+        {3, {7, 8, 9}},
+    } << endl;
+    os << map<int, tuple<int, vector<int>>>{
+        {1, {1, {1, 2, 3}}},
+        {2, {2, {4, 5, 6}}},
+        {3, {3, {7, 8, 9}}},
+    } << endl;
+    // uninsertable types
+    os << os << endl;
+    os << function<void(int)>{} << endl;
+    os << cout << endl;
+    os << cin << endl;
 }
